376586e3f07575b1765ad6b10862358de4eeded7
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-30 23:48:57 -0300

    Finalizando a service do projeto

--- cadastro-usuario/src/main/java/com/cadastro_usuario/business/UsuarioService.java
+++ cadastro-usuario/src/main/java/com/cadastro_usuario/business/UsuarioService.java
@@ -13,19 +13,27 @@ public class UsuarioService {
         this.repository = repository;
     }
 
-    public void salvarUsuario(Usuario usuario){
+    public void salvarUsuario(Usuario usuario) {
         repository.saveAndFlush(usuario);
     }
 
-    public Usuario buscarUsuarioPorEmail(String email){
-        return repository.findByEmail(email).orElseThrow(
-                () -> new RuntimeException("Email não encontrado!")
-        );
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return repository.findByEmail(email).orElseThrow(() -> new RuntimeException("Email não encontrado!"));
     }
 
-    public void deletaByEmail(String email){
+    public void deletaByEmail(String email) {
         repository.deleteByEmail(email);
     }
 
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder().email(usuario.getEmail() != null ? usuario.getEmail()
+                        : usuarioEntity.getEmail())
+                .name(usuario.getName() != null ? usuario.getName()
+                        : usuarioEntity.getName())
+                .id(usuarioEntity.getId())
+                .build();
 
+        repository.saveAndFlush(usuarioAtualizado);
+    }
 }
